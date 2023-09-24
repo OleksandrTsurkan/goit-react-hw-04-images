1483507993adf4e4c11434f7eaba59a9d9c9e60f
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-09-24 17:30:10 +0300

    start3

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,35 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Modaldiv, Overlay } from './Modal.styled';
+import React from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
-export class Modal extends Component {
-  componentDidMount() {
+
+export const Modal = ({ selectedPhoto: { src, alt }, onClose }) => {
+  useEffect(() => {
+    const handleKeyDawn = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', this.handleKeyDawn);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDawn);
-  }
-  handleKeyDawn = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  handleBackdropClick = e => {
+    return () => {
+      window.removeEventListener('keydown', handleKeyDawn);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const {
-      selectedPhoto: { src, alt },
-    } = this.props;
-
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <Modaldiv>
-          <img src={src} alt={alt} />
-        </Modaldiv>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <Modaldiv>
+        <img src={src} alt={alt} />
+      </Modaldiv>
+    </Overlay>,
+    modalRoot
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,52 +1,50 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ButtonSearch, FormInput, FormSearch, Searchbardiv } from './Searchbar.styled';
+import {
+  ButtonSearch,
+  FormInput,
+  FormSearch,
+  Searchbardiv,
+} from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  state = {
-    value: '',
+import React from 'react';
+
+export const Searchbar = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = ({ target: { value } }) => {
+    setValue(value);
   };
 
-  handleChange = ({ target: { value } }) => {
-    this.setState({ value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.value.trim() === '') {
+    if (value.trim() === '') {
       toast.info('Введіть свій запит, будь ласка!');
       return;
     }
-    this.props.onSubmit(this.state.value);
-    this.reset();
+    onSubmit(value);
+    reset();
   };
 
-  reset = () =>
-    this.setState({
-      value: '',
-    });
+  const reset = () => setValue('');
 
-  render() {
-    const { value } = this.state;
-    return (
-      <Searchbardiv>
-        <FormSearch onSubmit={this.handleSubmit}>
-          <FormInput
-            type="text"
-            onChange={this.handleChange}
-            value={value}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-          <ButtonSearch type="submit">
-            <span>Search</span>
-          </ButtonSearch>
-        </FormSearch>
-      </Searchbardiv>
-    );
-  }
-}
-
+  return (
+    <Searchbardiv>
+      <FormSearch onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          onChange={handleChange}
+          value={value}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <ButtonSearch type="submit">
+          <span>Search</span>
+        </ButtonSearch>
+      </FormSearch>
+    </Searchbardiv>
+  );
+};
 export default Searchbar;
