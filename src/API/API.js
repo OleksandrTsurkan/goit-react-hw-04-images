dd55efb87d73d1fdad99422ccab31e6c614c3292
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-09-25 16:48:11 +0300

    finish 1

--- src/API/API.js
+++ src/API/API.js
@@ -4,7 +4,12 @@ const API_KEY = '38678956-a1561c04372c108d76606b5d7';
 export const searchPhoto = (searchValue, page) => {
   return fetch(
     `${BASE_URL}?q=${searchValue}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  );
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
 
 export default searchPhoto;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,12 +24,6 @@ export const App = () => {
     }
     setIsloading(true);
     searchPhoto(photoName, page)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
       .then(data => {
         if (data.totalHits === 0) {
           toast.warning(
